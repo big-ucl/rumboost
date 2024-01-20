b30e76e4ab3842ebe9a43a9f243e08b9412de064
Nicolas Salvade <ucesnjs@ucl.ac.uk>
2024-01-20 23:27:38 +0000

    pypi working :)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Gradient Boosting Decision Trees for Random Utility Models'
 LONG_DESCRIPTION = 'A package that allows to estimate some random utility models through gradient boosting decision trees'
 
@@ -15,9 +15,17 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     url="https://github.com/NicoSlvd/rumboost",
-    install_requires=['lightgbm', 'matplotlib', 'seaborn', 'numpy', 
-                      'pandas', 'scikit-learn', 'scipy', 'biogeme',
-                      'sphinx_rtd_theme'],
+    install_requires=[
+        "biogeme==3.2.11",
+        "hyperopt==0.2.7",
+        "lightgbm==3.3.5",
+        "matplotlib==3.7.1",
+        "numpy==1.24.3",
+        "pandas==2.0.2",
+        "scikit-learn==1.2.2",
+        "scipy==1.10.1",
+        "seaborn==0.12.2"
+    ],
     keywords=['python', 'gbdt', 'rum', 'ml'],
     classifiers=[
         "Development Status :: 1 - Planning",
